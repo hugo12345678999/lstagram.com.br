8795afeac98796f401b0ac8c093c5b3fa8b8ad50
hugo12345678999 <hugovendpro@gmail.com>
2024-06-12 19:30:21 -0300

    first commit

--- reels/teste.py
+++ reels/teste.py
@@ -1,57 +1,38 @@
-from flask import Flask, request, jsonify
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-app = Flask(__name__)
+# Inicializa o WebDriver do Chrome
+driver = webdriver.Chrome()
 
-# Rota para lidar com o login
-@app.route('/login', methods=['POST'])
-def login():
-    data = request.get_json()
-    email = data.get('email')
-    password = data.get('password')
+try:
+    # Abre o Instagram
+    driver.get('https://www.instagram.com/accounts/login/')
 
-    # Inicializa o WebDriver do Chrome
-    driver = webdriver.Chrome()
+    # Aguarda a página carregar
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.NAME, 'username'))
+    )
 
-    try:
-        # Abre o Instagram
-        driver.get('https://www.instagram.com/accounts/login/')
+    # Encontra os campos de entrada e insere as credenciais
+    username = driver.find_element(By.NAME, 'username')
+    password = driver.find_element(By.NAME, 'password')
 
-        # Aguarda a página carregar
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.NAME, 'username'))
-        )
+    username.send_keys('mahiiu66')  # Substitua 'seu_usuario' pelo seu nome de usuário
+    password.send_keys('Maria*280810*')    # Substitua 'sua_senha' pela sua senha
 
-        # Encontra os campos de entrada e insere as credenciais
-        username = driver.find_element(By.NAME, 'username')
-        password_field = driver.find_element(By.NAME, 'password')
+    # Submete o formulário
+    password.send_keys(Keys.RETURN)
 
-        username.send_keys(email)  # Insere o email
-        password_field.send_keys(password)    # Insere a senha
+    # Aguarda o login completar
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//a[contains(@href, '/direct/inbox/')]"))
+    )
 
-        # Submete o formulário
-        password_field.send_keys(Keys.RETURN)
+    print("Login bem-sucedido!")
 
-        # Aguarda o login completar
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//a[contains(@href, '/direct/inbox/')]"))
-        )
-
-        print("Login bem-sucedido!")
-
-        return jsonify({"message": "Login bem-sucedido!"}), 200
-
-    except Exception as e:
-        print("Erro durante o login:", e)
-        return jsonify({"error": "Ocorreu um erro durante o login."}), 500
-
-    finally:
-        # Fecha o navegador
-        driver.quit()
-
-if __name__ == '__main__':
-    app.run(debug=True)
+finally:
+    # Fecha o navegador
+    driver.quit()
